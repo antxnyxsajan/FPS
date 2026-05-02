d1f477faaae2ebf1c2a9efd839090c77c0d9848f
Antony <antonysajant@gmail.com>
2026-05-02 16:08:13 +0530

    movement added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System;
+///using System.Numerics;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed=5f;
+    CharacterController controller;
+    PlayerControls pc;
+
+    void Awake()
+    {
+       controller=GetComponent<CharacterController>(); 
+       pc=new PlayerControls();
+       pc.Enable();
+    }
+
+    void Start()
+    {       }
+
+    void Update()
+    {
+        Vector2 input=pc.Player.Move.ReadValue<Vector2>();
+        Vector3 move=transform.right*input.x + transform.forward*input.y;
+        controller.Move(move*Time.deltaTime*speed);
+    }
+}
